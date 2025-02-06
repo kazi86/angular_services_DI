30b64520b397a641884c2ae4a2d6f2a3ce038a15
Huzaifa <muhammad.huzaifa.tariq@codingkey.com>
2025-02-06 14:09:45 +0500

    Adding Navigation to the Application

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,36 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector:'app-recipe-edit',
   templateUrl:'recipe-edit.component.html',
   styleUrls:['recipe-edit.component.css']
 })
-export class RecipeEditComponent{}
+export class RecipeEditComponent implements OnInit{
+
+  public recipeId !: number;
+
+  public editMode : boolean = false;
+
+  constructor(private route : ActivatedRoute) {}
+
+  public ngOnInit() {
+
+    this.route.params.subscribe({
+      next:(result)=>{
+        this.recipeId = +result['id'];
+        this.editMode = result['id'] !== null;
+        console.log(this.editMode);
+      },
+      error:(err)=>{
+        console.error('Unable to get current route info');
+      },
+      complete:()=>{
+
+      }
+    });
+
+  }
+
+
+}
